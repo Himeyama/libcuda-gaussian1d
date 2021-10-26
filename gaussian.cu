73fed6721507793c6a38fda08d0635c3baf7bece
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-10-27 03:07:14 +0900

    gauss

--- gaussian.cu
+++ gaussian.cu
@@ -6,15 +6,19 @@ template <typename T>
 void gaussian(std::vector<T> src, T truncate, T sd){
     long r = (long)(truncate * sd + 0.5);
     std::vector<T> data(src.size() + 2 * r);
+    
+    // Gaussian distribution
     std::vector<T> gauss(2 * r + 1);
     for(long i = -r; i <= r; i++)
         gauss[i + r] = exp(-0.5 * i * i / (sd * sd));
+    T gauss_sum = std::accumulate(gauss.begin(), gauss.end(), (T)0);
+    for(long i = 0; i < gauss.size(); i++)
+        gauss[i] /= gauss_sum; // Normalization
 
-    
 
-    // for(T e: gauss){
-    //     std::cout << e << std::endl;
-    // }
+    for(T e: gauss){
+        std::cout << e << std::endl;
+    }
 }
 
 int main(){
